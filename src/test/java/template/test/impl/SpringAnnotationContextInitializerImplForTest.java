4c3e48d8c4bf965e58878dbcbce9bcb61105edfb
romansen47 <romansen47@gmail.com>
2026-09-07 17:41:21 +0200

    Use valid test scan package

--- src/test/java/template/test/impl/SpringAnnotationContextInitializerImplForTest.java
+++ src/test/java/template/test/impl/SpringAnnotationContextInitializerImplForTest.java
@@ -1,27 +1,21 @@
 package template.test.impl;
 
-import org.springframework.context.annotation.ComponentScan;
-
 import template.annotation.initializer.SpringAnnotationContextInitializer;
 
 /**
- * Concrete implementation of the annotation context initializer.
- * 
- * Root package is template, only sub-packages are scanned for bean definition pick up.
- * @author romansen47
+ * Test implementation that scans the complete {@code template.test} package.
  */
-@ComponentScan(basePackages = SpringAnnotationContextInitializerImplForTest.BASE_PACKAGES)
 public class SpringAnnotationContextInitializerImplForTest extends SpringAnnotationContextInitializer {
 
-	public static final String BASE_PACKAGES = "template..*";
+    public static final String BASE_PACKAGES = "template.test";
 
-	/**
-	 * Returns the base packages.
-	 * @return the base packages
-	 */
-	@Override
-	public String getBasePackages() {
-		return SpringAnnotationContextInitializerImplForTest.BASE_PACKAGES;
-	}
-
+    /**
+     * Returns the package used by the integration tests.
+     *
+     * @return {@value #BASE_PACKAGES}
+     */
+    @Override
+    protected String getBasePackages() {
+        return BASE_PACKAGES;
+    }
 }
